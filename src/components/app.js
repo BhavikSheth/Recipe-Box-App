a451de528970a372c34bcd90de8b51e3d2113068
Bhavik Sheth <bhaviksheth20@gmail.com>
2018-01-29 13:44:19 +0000

    add header, footer, and css styling

--- src/components/app.js
+++ src/components/app.js
@@ -5,7 +5,9 @@ export default class App extends Component {
   render() {
     return (
       <div>
+        <h2 className="text-center text-primary">Recipe Box App</h2>
         <RecipeList />
+        <h6 className="text-center">Created by <a href="https://bhaviksheth.github.io" target="__blank">Bhavik Sheth</a></h6>
       </div>
     );
   }
